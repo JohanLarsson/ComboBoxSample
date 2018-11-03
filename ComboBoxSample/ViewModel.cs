bd2ee90ffb646990f0ea42cd95daaf73b1696779
JohanLarsson <johan_gavle1@hotmail.com>
2018-11-03 16:29:28 +0100

    Initial.

---
+++ ComboBoxSample/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿namespace ComboBoxSample
+{
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string selectedCurrency;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string SelectedCurrency
+        {
+            get => this.selectedCurrency;
+            set
+            {
+                if (value == this.selectedCurrency)
+                {
+                    return;
+                }
+
+                this.selectedCurrency = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<string> Currencies { get; } = new ObservableCollection<string> { "USD", "SEK" };
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
